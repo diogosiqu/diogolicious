1c5f826524b08f54ab12277465db5ec733455570
Diogo Siqueira <diogo@jn2.com.br>
2018-03-29 14:30:54 -0300

    list itens

--- src/the-container.vue
+++ src/the-container.vue
@@ -1,27 +1,41 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+    <div>
+      <li v-for="item in list" :key="item.key" class="list-itens">
+        {{item.bookmark_title}} <br>
+        <a :href="item.bookmark_link">{{ item.bookmark_link }}</a>
+        <p>{{ item.tag }}</p>
+      </li>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
-import { map } from 'lodash'
+// import _ from 'lodash' todo verificar documentaçao lodash
 export default {
   name: 'TheContainer',
   data () {
     return {
-      list: []
+      repos: [],
+      configs: {
+        // filter: '',
+        orderBy: 'bookmark_title'
+      }
     }
   },
   mounted () {
-    this.$db.ref('diogolicious-99d60').on('value', data => {
-      const obj = data.val()
-      this.list = map(obj, (site, index) => {
-        site.id = index
-        return site
-      })
+    this.$db.ref().on('value', snapshot => {
+      this.repos = snapshot.val()
     })
+  },
+  computed: {
+    list () {
+      // aki é onde a reatividade acontece - estudar reatividade https://www.youtube.com/watch?v=07-TvnH7XNo&list=PLcoYAcR89n-qq1vGRbaUiV6Q9puy0qigW
+      // const list = _.orderBy(this.repos, this.configs.orderBy)
+      return this.repos
+    }
   }
 }
 </script>
